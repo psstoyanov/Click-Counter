bfc1064140b2cdc47f38015506cbdc8997000e76
PakoSt <kokotekilata@gmail.com>
2018-02-21 17:07:30 +0000

    L1_E12: Transition from code to Storyboard
    Clear the created items from code and recreate them within Storyboard.
    We can see how the same connections are made there.

--- Click-Counter/ViewController.swift
+++ Click-Counter/ViewController.swift
@@ -11,48 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0;
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
 
 
     override func viewDidLoad() {
         super.viewDidLoad();
         
-        // Label
-        var label = UILabel();
-        label.frame  = CGRect.init(x: 150, y: 150, width: 60, height: 60);
-        label.text = "0";
-        
-        self.view.addSubview(label);
-        self.label = label;
-        
-        // Button Increment
-        var buttonIncrement = UIButton();
-        buttonIncrement.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60);
-        buttonIncrement.setTitle("Increment", for: .normal);
-        buttonIncrement.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping;
-        buttonIncrement.setTitleColor(UIColor.blue, for: .normal);
-        
-        self.view.addSubview(buttonIncrement);
-        buttonIncrement.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside);
-        
-        // Button Increment
-        var buttonDecrement = UIButton();
-        buttonDecrement.frame = CGRect.init(x: 150, y: 350, width: 60, height: 60);
-        buttonDecrement.setTitle("Decrement", for: .normal);
-        buttonDecrement.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping;
-        buttonDecrement.setTitleColor(UIColor.blue, for: .normal);
-        
-        self.view.addSubview(buttonDecrement);
-        buttonDecrement.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside);
-        
     }
     
-    @objc func incrementCount() {
+    func incrementCount() {
         self.count = self.count + 1
         self.label.text = "\(self.count)"
     }
     
-    @objc func decrementCount() {
+    func decrementCount() {
         self.count = self.count - 1
         self.label.text = "\(self.count)"
     }
