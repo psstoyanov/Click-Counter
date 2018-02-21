412cfba3b98b77bdae694da267196c7e21557029
PakoSt <kokotekilata@gmail.com>
2018-02-21 16:38:33 +0000

    L1_E5: Add a button and label for Click Counter

--- Click-Counter/ViewController.swift
+++ Click-Counter/ViewController.swift
@@ -11,8 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
+        
+        // Label
+        var label = UILabel();
+        label.frame  = CGRect.init(x: 150, y: 150, width: 60, height: 60);
+        label.text = "0";
+        
+        // Button
+        var button = UIButton();
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60);
+        button.setTitle("Click", for: .normal);
+        button.setTitleColor(UIColor.blue, for: .normal);
+        
+        // Add subviews.
+        self.view.addSubview(label);
+        self.view.addSubview(button);
     }
 
     override func didReceiveMemoryWarning() {
